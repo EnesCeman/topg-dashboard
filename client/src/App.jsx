89d69c68f8ae0ca7fbb866f1e12a7a71922decdf
Enes Ceman <Enes Ceman@epicentar.local>
2023-09-26 17:50:04 +0200

    baseurl update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import DateCards from "./containers/DateCards/DateCards";
 import axios from "axios";
 import { DataProvider } from "./store/DataContext";
 
-axios.defaults.baseURL = "https://topg-backend.onrender.com/";
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
