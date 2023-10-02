409df96c42be95f5c4a2ce66feacf4e6d8c02bbb
Enes Ceman <Enes Ceman@epicentar.local>
2023-10-02 12:22:12 +0200

    pool-connection

--- api/configs/pg_pool.js
+++ api/configs/pg_pool.js
@@ -2,13 +2,6 @@ const Pool = require("pg-pool");
 
 require("dotenv").config();
 
-const pool = new Pool({
-  user: "data_user",
-  host: "dpg-cka4d0ddrqvc73baugi0-a.oregon-postgres.render.com",
-  database: "dataddo_test",
-  password: "4LjOary9C6GCqaTB7axGX9nUznJd7g6Q",
-  port: "5432",
-  ssl: "true",
-});
+const pool = new Pool(JSON.parse(process.env.POOL_CONNECTION));
 
 module.exports = pool;
