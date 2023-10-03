3a9fbb6d2854d8547b0ec5b0b520f440c3f3a687
Enes Ceman <Enes Ceman@epicentar.local>
2023-10-03 18:52:37 +0200

    cors whitelist2

--- api/server.js
+++ api/server.js
@@ -28,7 +28,7 @@ app.use(
       "https://topg-dashboard.onrender.com",
       "https://topg-dashboard.vercel.app",
       "http://localhost:5173",
-      "emp-dashboard-demo.vercel.app",
+      "https://emp-dashboard-demo.vercel.app",
     ],
   })
 );
